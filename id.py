7a9e0f702cceab09cc6e941ba1bc333f6acb9a5a
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-06-19 10:59:05 +0530

    Update id.py

--- id.py
+++ id.py
@@ -1,28 +1,28 @@
-#hari
+#Gowtham
 import sys, string, math
-s1,s2 = input().split()
+s0,s1 = input().split()
+n0 = len(s0)
 n1 = len(s1)
-n2 = len(s2)
-if n2 > n1 :
+if n1 > n0 :
     i = 0
-    while i<n1 and s1[i] == s2[i] :
+    while i<n0 and s0[i] == s1[i] :
         i += 1
-    print(n2-i)
-elif n2 == n1 :
+    print(n1-i)
+elif n1 == n0 :
     i = 0
-    while i<n2 and s1[i] == s2[i] :
+    while i<n1 and s0[i] == s1[i] :
         i += 1
-    print(n2-i)
+    print(n1-i)
 else :
     i = 0
-    while i<n2 and s1[i] == s2[i] :
+    while i<n1 and s0[i] == s1[i] :
         i += 1
-    s31 = s1[i:]
-    s32 = s2[i:]
+    s31 = s0[i:]
+    s32 = s1[i:]
     L = list(s31)
     k = 0
     for c in s32 :
         if c in L :
             k += 1
             L.remove(c)
-    print(n1-i-k)
+    print(n0-i-k)
